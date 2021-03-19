5b2abfeca466d3185a5aa0a717b6b99ae338e91a
iskander1996 <mukataev_1996@mail.ru>
2021-03-19 17:01:03 +0600

    I put second subplot

--- plots2.py
+++ plots2.py
@@ -12,7 +12,17 @@ x = np.arange(-10.0, 10.01, 0.01)
 t = np.arange(-10, 11, 1)
 
 #subplot 1
-sp = plt.subplot(221)
+sp = plt.subplot(2,2,1)
 plt.plot(x, np.sin(x))
 plt.title(r'$sin(x)$')
 plt.grid(True)
+
+#subplot 2
+
+sp = plt.subplot(2,2,2)
+plt.plot(x, np.cos(x), 'g')
+plt.axis('equal')
+plt.grid(True)
+plt.title(r'$cos(x)$')
+
+#subplot 3
