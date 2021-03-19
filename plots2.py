09d8d1e0439cf8f5c437c03fc956136b4f3a4295
iskander1996 <mukataev_1996@mail.ru>
2021-03-19 16:48:46 +0600

    Added new file

---
+++ plots2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 19 16:41:55 2021
+
+@author: iskander
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.range(-10.0, 10.01, 0,01)
+t = np.range(-10, 11, 1)
+
+#subplot 1
+
+#subplo
